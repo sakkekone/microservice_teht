50c147039bf28657d84cd56adb6595307406694e
sakke <dsavu004@edu.xamk.fi>
2024-03-04 13:04:37 +0200

    pagination assignment done

--- microservice_teht/Controllers/ValuesController.cs
+++ microservice_teht/Controllers/ValuesController.cs
@@ -2,7 +2,6 @@
 using microservice_teht.Extensions;
 using microservice_teht.models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
 
 namespace microservice_teht.Controllers
@@ -25,12 +24,6 @@ namespace microservice_teht.Controllers
             _electricityDbContext = electricityDbContext;
             _logger = logger;
             _httpClientFactory = httpClientFactory;
-        }
-
-        [HttpGet]
-        public string test_get()
-        {
-            return ("test return");
         }
 
         [Route("getdb")]
@@ -87,9 +80,9 @@ namespace microservice_teht.Controllers
             _logger.LogInformation("added " + counter + " new entries");
             //_electricityDbContext.ElectricityPriceDatas.OrderBy(e => e.StartDate);
             await _electricityDbContext.SaveChangesAsync();
-            return data2.Prices.Count+" entries fetched, not adding duplicates: "+counter.ToString()+" new entries added";
+            return data2.Prices.Count + " entries fetched, not adding duplicates: " + counter.ToString() + " new entries added";
         }
-        
+
         [Route("cleartable")]
         [HttpGet]
         public async Task<IActionResult> clear_table()
@@ -107,9 +100,10 @@ namespace microservice_teht.Controllers
             _logger.LogInformation("table cleared");
             return Ok("table cleared");
         }
+
         [Route("getprices")]
         [HttpGet]
-        public async Task<IActionResult> get_prices([FromQuery] DateTime start,[FromQuery] DateTime end)
+        public async Task<IActionResult> get_prices([FromQuery] DateTime? start,[FromQuery] DateTime? end)
         {
             if(start == null || end == null)
             {
@@ -118,16 +112,111 @@ namespace microservice_teht.Controllers
             string log = "entries between " + start + " and " + end + "\n";
             try
             {
-                var linq = _electricityDbContext.ElectricityPriceDatas
+                var result = _electricityDbContext.ElectricityPriceDatas
                     .Where(e => (e.StartDate > start && e.StartDate < end))
                     .OrderBy(e => e.StartDate).ToList();
-                foreach(var a in linq)
+                foreach(var a in result)
                 {
                     log += a.StartDate + ":\t" + a.Price + "\n";
                 }
                 _logger.LogInformation(log);
-                _logger.LogInformation("total entries: " + linq.Count.ToString());
-                return Ok(log+"total entries: "+linq.Count.ToString());
+                _logger.LogInformation("total entries: " + result.Count.ToString());
+                return Ok(result);
+            } catch(Exception e)
+            {
+                _logger.LogError(e,"error!");
+                return StatusCode(StatusCodes.Status500InternalServerError,"error");
+            }
+        }
+
+        [Route("getprices_page")]
+        [HttpGet]
+        public async Task<IActionResult> get_prices_page([FromQuery] DateTime? start,[FromQuery] DateTime? end,
+            [FromQuery] int page,[FromQuery] int pageSize)
+        {
+            if(page < 1) { page = 1; }
+            if(pageSize < 1) { pageSize = 1; }
+            if(pageSize > 20) { pageSize = 20; }//max pagesize 20
+            if(start == null || end == null)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest,"start and end parameters must be in DateTime format");
+            }
+            string log = "entries between " + start + " and " + end + "\n";
+            try
+            {
+                var result = _electricityDbContext.ElectricityPriceDatas
+                    .Where(e => (e.StartDate > start && e.StartDate < end))
+                    .OrderBy(e => e.StartDate)
+                    .Skip((page - 1) * pageSize)
+                    .Take(pageSize).ToList();
+
+                foreach(var a in result)
+                {
+                    log += a.StartDate + ":\t" + a.Price + "\n";
+                }
+                log += "page: " + page + " pageSize: " + pageSize;
+                _logger.LogInformation(log);
+                return Ok(result);
+            } catch(Exception e)
+            {
+                _logger.LogError(e,"error!");
+                return StatusCode(StatusCodes.Status500InternalServerError,"error");
+            }
+        }
+        [Route("GetPriceDifference")]//returns total priceDifference between start and end compared to fixedPrice, negative number means fixedPrice is cheaper
+        [HttpGet]
+        public async Task<IActionResult> get_prices_dif([FromQuery] DateTime start,[FromQuery] DateTime end,
+            [FromQuery] decimal fixedPrice)
+        {
+            if(start == null || end == null)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest,"start and end parameters must be in DateTime format");
+            }
+            string log = "price difference between " + start + " and " + end + "\tfixedPrice:" + fixedPrice + "\n";
+            try
+            {
+                var result = _electricityDbContext.ElectricityPriceDatas
+                    .Where(e => (e.StartDate > start && e.StartDate < end))
+                    .OrderBy(e => e.StartDate).ToList();
+                decimal totalprice = 0;
+                foreach(var a in result)
+                {
+                    totalprice += a.Price;
+                }
+                totalprice /= result.Count;
+                log += "totalprice: " + totalprice + "\n";
+                totalprice -= fixedPrice;
+                _logger.LogInformation(log);
+                return Ok(new PriceDifference(start,end,totalprice));
+            } catch(Exception e)
+            {
+                _logger.LogError(e,"error!");
+                return StatusCode(StatusCodes.Status500InternalServerError,"error");
+            }
+        }
+        [Route("GetPriceDifferenceList")]//returns list of priceDifference for each hour between start and end, negative number means 
+        [HttpGet]
+        public async Task<IActionResult> get_prices_dif_list([FromQuery] DateTime? start,[FromQuery] DateTime? end,
+            [FromQuery] decimal fixedPrice)
+        {
+            if(start == null || end == null)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest,"start and end parameters must be in DateTime format");
+            }
+            string log = "price difference between " + start + " and " + end + "\tfixedPrice:" + fixedPrice + "\n";
+            try
+            {
+                var result = _electricityDbContext.ElectricityPriceDatas
+                    .Where(e => (e.StartDate > start && e.StartDate < end))
+                    .OrderBy(e => e.StartDate).ToList();
+                List<PriceDifference> pd_list = new List<PriceDifference>();
+                foreach(var a in result)
+                {
+                    pd_list.Add(new PriceDifference(a.StartDate,a.EndDate,a.Price - fixedPrice));
+                    log += a.StartDate + ":\t" + pd_list[pd_list.Count - 1].PriceDifferenceValue + "\n";
+                }
+                _logger.LogInformation(log);
+                return Ok(pd_list);
             } catch(Exception e)
             {
                 _logger.LogError(e,"error!");
---
+++ microservice_teht/classes/classes.cs
@@ -0,0 +1,19 @@
+﻿namespace microservice_teht
+{
+    public class PriceDifference
+    {
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public decimal PriceDifferenceValue { get; set; }
+
+        public bool fixedIsCheaper { get; set; }
+
+        public PriceDifference(DateTime startDate,DateTime endDate,decimal priceDifference)
+        {
+            StartDate = startDate;
+            EndDate = endDate;
+            PriceDifferenceValue = priceDifference;
+            fixedIsCheaper = (PriceDifferenceValue < 0);
+        }
+    }
+}
